e2d74f05a69767f541dba393d2986c8ec3542d53
Jason Stunkard <jstunkard@gmail.com>
2013-03-28 11:18:36 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddiesArt::Application.config.secret_token = 'c447be2588f1d16e28b171b4b8a82be3966103865e97e8b9fdeeb86d9f3c06cdcdaed4c726d04b9370433cce2278c1e16c67eeb3988670417f4649361a2dc645'
